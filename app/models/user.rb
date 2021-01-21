0fc7ba3a8a45b34cb5d8272dc0ed0b21014d71d6
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-01-21 18:39:06 -0500

    commit tree 6727eccfb9e7fd9d9afe52ee2815c308fcd84e2c

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) 
+  end
 end
